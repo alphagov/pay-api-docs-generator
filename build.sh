a8148cc6352df148e4624ac0cd7abc770ea03909
Krishna Bottla <krishna.bottla@digital.cabinet-office.gov.uk>
2019-02-01 11:04:06 +0000

    PP-4617 Build scripts

---
+++ build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# Set up term colors
+green='\033[1;32m'
+red='\033[0;31m'
+white='\033[1;37m'
+NC='\033[0m' # No Color
+
+# ---- clean up existing directories 
+rm -rf swagger
+rm -rf shins
+
+if ! which node > /dev/null; then \
+  echo -e "${red} Nodejs is not available, please install NodeJs (https://nodejs.org) ${NC}"
+  exit 1; \
+fi
+
+if ! which git > /dev/null; then \
+  echo -e "${red} git is not available, please install Git (https://git-scm.com) ${NC}"
+  exit 1; \
+fi
+
+# -- Get shins to build static files --
+git clone https://github.com/alphagov/shins.git
+
+# -- Get Swagger file --
+if [[ -z "${PUBLIC_API_BRANCH}" ]]; then
+    PUBLIC_API_BRANCH="master"
+else
+    PUBLIC_API_BRANCH="${PUBLIC_API_BRANCH}"
+fi;
+
+if [[ -z "${PUBLIC_API_URL}" ]]; then
+    PUBLIC_API_URL="https://raw.githubusercontent.com/alphagov/pay-publicapi/${PUBLIC_API_BRANCH}/swagger/swagger.json"
+else
+    PUBLIC_API_URL="${PUBLIC_API_URL}"
+fi;
+
+echo "Getting Swagger file from location '$PUBLIC_API_URL'"
+wget -P swagger $PUBLIC_API_URL
+
+# -- Build docs --
+echo -e "${white} Converting swagger file to shins compatible markdown...${NC}"
+cp source/images/govuk_pay_logo.png shins/source/images/logo.png
+npm install && ./node_modules/widdershins/widdershins.js swagger/swagger.json -o shins/source/index.html.md
+
+echo -e "${white} Building static docs...${NC}"
+cd shins && npm install && node shins.js --minify
+
+# -- Move static files to 'build' folder --
+mkdir -p build/source
+cp index.html build/
+cp -R pub/ build/pub
+cp -R source/* build/source
+echo -e "${green}Built pay-api-docs static files...${NC}"
