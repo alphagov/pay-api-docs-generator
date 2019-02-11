f54d8678e5a61def55446635ebf164c2e74c35d2
Krishna Bottla <krishna.bottla@digital.cabinet-office.gov.uk>
2019-02-11 12:05:14 +0000

    PP-4617 Dockerize Node.js scripts
    - Dockerised Node.js & build scripts so Jenkins doesn't need to be aware of Node.js / can be independent of the version

---
+++ docker-startup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+white='\033[1;37m'
+green='\033[1;32m'
+
+echo -e "${white} Converting swagger file to shins compatible markdown...${NC}"
+npm install
+./node_modules/widdershins/widdershins.js swagger/swagger.json -o shins/source/index.html.md
+
+
+echo -e "${white} Building static docs...${NC}"
+cd shins
+npm install
+node shins.js --minify
+
+# -- Move static files to 'build' folder --
+mkdir -p build
+cp -R index.html pub source build/
+
+echo -e "${green}Built pay-api-docs static files...${NC}"
